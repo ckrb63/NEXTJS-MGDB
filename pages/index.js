58132f690306fac70573d7e727ed1e94ec1c574f
cksrb63 <cksr1@naver.com>
2022-03-29 18:07:10 +0900

    api setting

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -6,36 +7,49 @@ const DUMMY_MEETUPS = [
     title: "A Forst Meetup",
     image: "https://src.hidoc.co.kr/image/lib/2021/4/28/1619598179113_0.jpg",
     address: "Some address 5, 12345 Some city",
-    description: "This is a first meetup!",
+    description: "This is a first meetup!"
   },
   {
     id: "m2",
     title: "A Forst Meetup",
     image: "https://src.hidoc.co.kr/image/lib/2021/4/28/1619598179113_0.jpg",
     address: "Some address 5, 12345 Some city",
-    description: "This is a second meetup!",
+    description: "This is a second meetup!"
   },
   {
     id: "m3",
     title: "A Forst Meetup",
     image: "https://src.hidoc.co.kr/image/lib/2021/4/28/1619598179113_0.jpg",
     address: "Some address 5, 12345 Some city",
-    description: "This is a third meetup!",
-  },
+    description: "This is a third meetup!"
+  }
 ];
 
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export function getStaticProps(){
-  //  fetch data from API
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://dbuser:asd123@cluster0.xnekx.mongodb.net/meetup?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupCollection = db.collection("meetups");
+  const meetups = await meetupCollection.find().toArray();
   return {
-    props:{
-      meetups: DUMMY_MEETUPS
+    props: {
+      meetups: meetups.map((meetup) => {
+        return {
+          id: meetup._id.toString(),
+          title: meetup.title,
+          image: meetup.image,
+          address: meetup.address,
+          description: meetup.description
+        };
+      })
     },
-    revalidate:2
+    revalidate: 2
   };
-};
+}
 
 export default HomePage;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,18 +1,21 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler =  async (entered) => {
-    const response = await fetch('/api/new-meetup',{
-      method:'POST',
+  const router = useRouter();
+  const addMeetupHandler = async (entered) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
       body: JSON.stringify(entered),
       headers: {
-        'Content-Type' : 'application/json'
+        "Content-Type": "application/json"
       }
     });
     const data = await response.json();
     console.log(data);
+    router.push("/");
   };
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
 export default NewMeetupPage;
